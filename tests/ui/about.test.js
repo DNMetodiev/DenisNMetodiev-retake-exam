6389551b02296b749ce163cb743b57208018599f
DNMetodiev <91205942+DNMetodiev@users.noreply.github.com>
2023-08-18 11:57:19 +0300

    fix another /

--- tests/ui/about.test.js
+++ tests/ui/about.test.js
@@ -1,7 +1,7 @@
 const { test, expect } = require('@playwright/test');
 
 test('Check about page', async ({ page }) => {
-  await page.goto('https://softuni-denisnmetodiev-library.onrender.com//about');
+  await page.goto('https://softuni-denisnmetodiev-library.onrender.com/about');
   const heading = await page.waitForSelector('h1');
   const text = await heading.textContent();
   expect(text).toBe('About');
